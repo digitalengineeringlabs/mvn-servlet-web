4113820af6ea45538a7f44924fdf39b1263569da
Vimal Krishnan <akvimal@gmail.com>
2021-07-29 01:30:54 +0530

    REST

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -15,10 +15,13 @@ public class HelloServlet extends HttpServlet {
 		
 		System.out.println("processing request ...");
 		
+		int i = 1/0;
+		
 		response.setContentType("text/html"); 
 
-		String[] names = {"Mike","Kevin"};
+		String[] names = {"Mike","Kevin","Peter"};
 		request.setAttribute("names", names);
+		
 		
 		//Forwarding to JSP (Java Server Pages)
 		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
